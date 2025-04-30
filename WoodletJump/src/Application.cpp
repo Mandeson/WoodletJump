15d7158f6b83923cc4dd1210afe79f7c6cc6f538
Tomasz Klimek <tomkl584@gmail.com>
2025-04-30 16:52:22 +0200

    Fix Application to properly handle all exceptions

--- WoodletJump/src/Application.cpp
+++ WoodletJump/src/Application.cpp
@@ -10,33 +10,33 @@ void Application::run() {
 
     try {
         game_ = std::make_unique<WoodletJump>();
-    } catch (WoodletJump::InitError &e) {
+
+        game_->windowSize({static_cast<int>(kInitialWindowSize.x),
+            static_cast<int>(kInitialWindowSize.y)});
+
+        while (window_.isOpen())
+        {
+            while (const std::optional event = window_.pollEvent())
+            {
+                if (event->is<sf::Event::Closed>()) {
+                    game_.reset();
+                    window_.close();
+                    return;
+                }
+                else if (const auto* resized = event->getIf<sf::Event::Resized>()) {
+                    auto [x, y] = resized->size;
+                    glViewport(0, 0, x, y);
+                    game_->windowSize({static_cast<int>(x), static_cast<int>(y)});
+                }
+            }
+
+            game_->render();
+
+            window_.display();
+        }
+    } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
         window_.close();
         return;
-    }
-
-    game_->windowSize({static_cast<int>(kInitialWindowSize.x),
-            static_cast<int>(kInitialWindowSize.y)});
-
-    while (window_.isOpen())
-    {
-        while (const std::optional event = window_.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>()) {
-                game_.reset();
-                window_.close();
-                return;
-            }
-            else if (const auto* resized = event->getIf<sf::Event::Resized>()) {
-                auto [x, y] = resized->size;
-                glViewport(0, 0, x, y);
-                game_->windowSize({static_cast<int>(x), static_cast<int>(y)});
-            }
-        }
-
-        game_->render();
-
-        window_.display();
     }
 }
