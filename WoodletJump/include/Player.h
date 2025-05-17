6e4eef7ecfa77f92e0d3723ddc7ca2d94525df06
Tomasz Klimek <tomkl584@gmail.com>
2025-05-17 12:08:43 +0200

    Add timeStep method and simple movement controller to Player

--- WoodletJump/include/Player.h
+++ WoodletJump/include/Player.h
@@ -9,8 +9,9 @@ public:
     Player();
     void init();
     void render(Renderer::SpriteRenderer &sprite_renderer, Vector2i window_size);
+    void timeStep(double d_time);
 private:
-    static constexpr TextureRect kPlayerBodyTextureRect = TextureRect(Constants::kTextureSize, {0, 0}, {82, 82});
+    static constexpr TextureRect kPlayerBodyTextureRect = TextureRect(Constants::kTextureSize, {0, 0}, {81, 81});
     Sprite body_;
-    Vector2f position_;
+    Vector2d position_;
 };
--- WoodletJump/src/Player.cpp
+++ WoodletJump/src/Player.cpp
@@ -1,14 +1,28 @@
 #include <Player.h>
+#include <SFML/Window/Keyboard.hpp>
 
-Player::Player() : position_{0.1f, 0.8f} { }
+Player::Player() : position_{0.2f, 0.6f - 0.07f / 2} { }
 
 void Player::init() {
     body_.init(kPlayerBodyTextureRect);
 }
 
 void Player::render(Renderer::SpriteRenderer &sprite_renderer, Vector2i window_size) {
-    body_.setPosition({position_.x * window_size.y, position_.y * window_size.y});
+    body_.setPosition({static_cast<float>(position_.x * window_size.y), static_cast<float>(position_.y * window_size.y)});
     float pixel_size = 0.07f * window_size.y;
     body_.setSize({pixel_size, pixel_size});
     sprite_renderer.render(body_);
+}
+
+void Player::timeStep(double d_time) {
+    double move = d_time / 10;
+    bool right = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right);
+    bool left = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left);
+    bool jump = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z);
+
+    if (right && !left) {
+        position_.x += move;
+    } else if (left && !right) {
+        position_.x -= move;
+    }
 }
