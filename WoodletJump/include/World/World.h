22a6ad85d61f8fa0981095778a92028f25258ce5
Tomasz Klimek <tomkl584@gmail.com>
2025-05-19 12:32:37 +0200

    Add collision detection

--- WoodletJump/include/World/World.h
+++ WoodletJump/include/World/World.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <functional>
 #include <World/Platform.h>
 #include <Camera.h>
 
@@ -10,7 +11,10 @@ class World {
 public:
     World();
     void buildMesh(BufferBuilder &buffer_builder, Vector2i window_size, const Camera &camera) const;
+    bool checkCollision(const Camera &camera, Box object) const;
 private:
+    // Executes lambda for every platform within the given camera view, as long as lambda returns true. If lambda returns false, forEachVisiblePlatform returns.
+    void forEachVisiblePlatform(const Camera &camera, std::function<bool(const Platform &)> lambda) const;
     std::map<float, Platform> platforms_;
 };
 
--- WoodletJump/src/World/World.cpp
+++ WoodletJump/src/World/World.cpp
@@ -8,6 +8,30 @@ World::World() {
 }
 
 void World::buildMesh(BufferBuilder &buffer_builder, Vector2i window_size, const Camera &camera) const {
+    forEachVisiblePlatform(camera, [&buffer_builder, window_size](const Platform &platform) {
+        platform.buildMesh(buffer_builder, window_size);
+        return true; // continue the loop
+    });
+}
+
+bool World::checkCollision(const Camera &camera, Box object) const {
+    bool colision = false;
+    forEachVisiblePlatform(camera, [&object, &colision](const Platform &platform) {
+        Vector2f position = platform.getPosition();
+        Vector2f size = {platform.getWidth(), Platform::kPlatformMidSize};
+        position.x -= size.x;
+        if ((object.position.x + object.size.x > position.x && object.position.x < position.x + size.x)
+                && (object.position.y + object.size.y > position.y && object.position.y < position.y + size.y)) {
+            colision = true;
+            return false; // do not continue the loop
+        } else {
+            return true; // continue the loop
+        }
+    });
+    return colision;
+}
+
+void World::forEachVisiblePlatform(const Camera &camera, std::function<bool(const Platform &)> lambda) const {
     float camera_pos = static_cast<float>(camera.getPosition());
     float camera_bound = camera_pos + camera.getWidth();
     for (auto element = platforms_.lower_bound(camera_pos); element != platforms_.end(); element++) {
@@ -15,9 +39,9 @@ void World::buildMesh(BufferBuilder &buffer_builder, Vector2i window_size, const
             break;
         
         const auto &platform = element->second;
-        if (element->first - platform.getWidth() <= camera_bound) // do not draw if outside the screen
-            platform.buildMesh(buffer_builder, window_size);
+        if (element->first - platform.getWidth() <= camera_bound) // inside the camera view
+            if (!lambda(platform))
+                break;
     }
 }
-
 }
